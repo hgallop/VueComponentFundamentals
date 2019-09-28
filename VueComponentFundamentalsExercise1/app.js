39dd03e53ab2f03596875c49c83ce634871d9f47
Heather Gallop <doyouseeitmyway@gmail.com>
2019-09-28 13:35:56 -0400

    Begin Set Up Ex 1
    Add call to app.js script. Move call to create new Vue object to js file.  Add snippet to explore response from github api.

---
+++ VueComponentFundamentalsExercise1/app.js
@@ -0,0 +1,8 @@
+axios.get('https://api.github.com/users/hgallop')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
